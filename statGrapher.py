7d58b919b48d8b82e7aaff180013d237f4cd66b2
Rory <rory.allen1@btinternet.com>
2018-08-24 11:20:21 +0100

    added populate index and made whole graph drawer work

--- statGrapher.py
+++ statGrapher.py
@@ -2,9 +2,24 @@
 
 import csv
 from turtle import Turtle
+import turtle
+import time
+
+screen = turtle.Screen()
+screen.tracer(0,0)
 
 csvRead = open("data.csv")
 csvRead = csv.reader(csvRead)
+
+def populateIndex(index):
+    write = open("data.csv","w")
+    write = csv.writer(write)
+    def multiplyIndex(i,index):
+        for j in range(index-1):
+            i *= i
+        return i
+    for i in range(100):
+        write.writerow([i,multiplyIndex(i,index)])
 
 def initTurtle(name,colour):
     name.penup()
@@ -31,31 +46,39 @@ def getRowCount():
         rowCount += 1
     return rowCount
 
-def initData():
+def initData(csvRead):
     xAxis = []
     yAxis = []
     for row in csvRead:
-        xAxis.append(row[0])
-        yAxis.append(row[1])
+        xAxis.append(int(row[0]))
+        yAxis.append(int(row[1]))
     return xAxis,yAxis
 
-xAxis = initData()[0]
-yAxis = initData()[1]
+def greatestValue(array):
+    value = array[0]
+    for i in array:
+        if i > value:
+            value = i
+    return value
 
-def determineIntervals(xAxis,yAxis):
-    xInterval = 600/len(xAxis)
-    yInterval = 600/len(yAxis)
-    return(xInterval,yInterval)
+populateIndex(2)
+bothAxis = initData(csvRead)
+xAxis = bothAxis[0]
+yAxis = bothAxis[1]
 
-xInterval = determineInterval[0]
-yInterval = determineInterval[1]
+xInterval = 600/len(xAxis)
+yMultiplier = 400 / greatestValue(yAxis)
 
 def drawGraph(turt):
     for i in range(len(xAxis)):
-        turt.goto(xInterval*i,yInterval*i)
+        turt.goto((xInterval*(i))-300,(yAxis[i]*yMultiplier)-200)
 
 drawAxis()
 graph = Turtle()
 initTurtle(graph,"red")
 goCentre(graph)
+drawGraph(graph)
 
+screen.update()
+time.sleep(5)
+screen.update()
